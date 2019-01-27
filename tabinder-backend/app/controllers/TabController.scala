fb3828aa48fb9566bfc49ff2702903de5bfb7c7a
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-01-27 19:01:09 +0000

    defined the algebras

--- tabinder-backend/app/controllers/TabController.scala
+++ tabinder-backend/app/controllers/TabController.scala
@@ -1,5 +1,15 @@
 package controllers
 
-class TabController {
+import models.Types.{Artist, SongName, Tuning}
+import play.api.mvc.{Action, AnyContent}
+import models._
 
+import scala.language.higherKinds
+
+trait TabController[F[_]] {
+  def post(): Action[AnyContent]
+  def delete(): Action[AnyContent]
+  def getByArtist(artist: Artist):   Action[AnyContent]
+  def getByTuning(tuning: Tuning):   Action[AnyContent]
+  def getBySong(songName: SongName): Action[AnyContent]
 }
--- tabinder-backend/app/services/TabService.scala
+++ tabinder-backend/app/services/TabService.scala
@@ -1,13 +1,13 @@
-package repositories
+package services
 
 import models.Tab
 import models.Types.{Artist, SongName, Tuning}
 
 import scala.language.higherKinds
 
-trait TabRepository[F[_]] {
-  def create(tab: Tab): F[Unit]
-  def remove(tab: Tab): F[Unit]
+trait TabService[F[_]] {
+  def post(tab: Tab): F[Unit]
+  def delete(tab: Tab): F[Unit]
   def getByArtist(artist: Artist):   F[List[Tab]]
   def getByTuning(tuning: Tuning):   F[List[Tab]]
   def getBySong(songName: SongName): F[List[Tab]]
