b767c33e17d08e6ecee5b3e34eb4a05edec8a465
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-01-30 15:22:57 +0000

    implicit ec

--- tabinder-backend/app/utils/FromFuture.scala
+++ tabinder-backend/app/utils/FromFuture.scala
@@ -6,13 +6,13 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.language.higherKinds
 
 trait FromFuture[F[_]] {
-  def fromFuture[R](f: => Future[R]): F[R]
+  def fromFuture[R](f: => Future[R])(implicit ec: ExecutionContext): F[R]
   def toFuture[R](f: => F[R])(implicit request: Request[AnyContent], ec: ExecutionContext): Future[R]
 }
 
 object FromFuture {
   implicit val futureFromFuture: FromFuture[Future] = new FromFuture[Future] {
-    override def fromFuture[R](f: => Future[R]): Future[R] = f
+    override def fromFuture[R](f: => Future[R])(implicit ec: ExecutionContext): Future[R] = f
 
     override def toFuture[R](f: => Future[R])(implicit request: Request[AnyContent], ec: ExecutionContext): Future[R] = f
   }
