adf87d79387eaba8f6fa2d5a35b55555e59631e9
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-02-04 16:14:39 +0000

    AppLoader

--- tabinder-backend/app/utils/AppLoader.scala
+++ tabinder-backend/app/utils/AppLoader.scala
@@ -2,31 +2,36 @@ package utils
 
 import ai.snips.bsonmacros.DatabaseContext
 import app.Routes
-import controllers.{HomeController, TabControllerImpl}
+import controllers.{HomeController, TabController, TabControllerImpl}
 import logger.{MLogger, PlayLogger}
 import play.api.ApplicationLoader.Context
 import play.api.mvc.EssentialFilter
 import play.api.routing.Router
 import cats.instances.future._
 import play.api.BuiltInComponentsFromContext
-import services.TabService
+import services.{TabService, TabServiceAlgebra}
 import com.softwaremill.macwire.wire
-import repositories.TabRepository
-
+import repositories.{TabRepository, TabRepositoryAlgebra}
+import utils.FromFuture._
 import scala.concurrent.{ExecutionContext, Future}
 
-class AppLoader(context: Context) extends BuiltInComponentsFromContext(context){
+class AppLoader(context: Context) extends BuiltInComponentsFromContext(context) {
   lazy val ec: ExecutionContext = actorSystem.dispatcher
 
   lazy val prefix: String           = "/"
-  lazy val appRouter: Routes = wire[Routes]
 
-  lazy val tabController: TabControllerImpl[Future] = wire[TabControllerImpl[Future]]
-  lazy val homeController: HomeController           = wire[HomeController]
-  lazy val service: TabService[Future]           = wire[TabService[Future]]
-  lazy val repository: TabRepository[Future]     = wire[TabRepository[Future]]
-  lazy val logger: MLogger[Future]               = wire[PlayLogger[Future]]
+  lazy val homeController: HomeController        = wire[HomeController]
+
   lazy val dbContent: DatabaseContext            = wire[DatabaseContext]
+  lazy val logger: MLogger[Future]               = wire[PlayLogger[Future]]
+
+  lazy val repository: TabRepositoryAlgebra[Future] = wire[TabRepository[Future]]
+
+  lazy val service: TabServiceAlgebra[Future]    = wire[TabService[Future]]
+
+  lazy val tabControllerImpl: TabController      = wire[TabControllerImpl[Future]]
+
+  lazy val appRouter: Routes = ???
 
   override lazy val router: Router = appRouter
 
