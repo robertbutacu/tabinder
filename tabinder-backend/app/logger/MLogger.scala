54de66396cf689e9d8fa57f1e3a95a2c68a3f689
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-03-16 22:10:06 +0000

    refactoring logger

--- tabinder-backend/app/logger/MLogger.scala
+++ tabinder-backend/app/logger/MLogger.scala
@@ -1,10 +1,9 @@
 package logger
 
 import cats.Monad
-import cats.effect.IO
 import javax.inject.Inject
 import play.api.Logger
-
+import cats.syntax.applicative._
 import scala.language.higherKinds
 
 trait MLogger[F[_]] {
@@ -13,16 +12,8 @@ trait MLogger[F[_]] {
   def warning(s: String): F[Unit]
 }
 
-class PlayLogger[F[_]] @Inject()(implicit M: Monad[F]) extends MLogger[F] {
-  override def error(s: String): F[Unit]   = M.pure {
-    Logger.error(s"[Error] $s")
-  }
-
-  override def message(s: String): F[Unit] = M.pure {
-    Logger.debug(s"[Message] $s")
-  }
-
-  override def warning(s: String): F[Unit] = M.pure {
-    Logger.warn(s"[Warning] $s")
-  }
+class PlayLogger[F[_]: Monad] @Inject() extends MLogger[F] {
+  override def error(s: String): F[Unit]   = Logger.error(s"[Error] $s").pure
+  override def message(s: String): F[Unit] = Logger.debug(s"[Message] $s").pure
+  override def warning(s: String): F[Unit] = Logger.warn(s"[Warning] $s").pure
 }
