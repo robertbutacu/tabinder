3ae413eb8787a6a4b9206c2441edd9644dad3583
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-03-26 11:30:52 +0000

    testing

--- tabinder-backend/test/concrete/TabGenerator.scala
+++ tabinder-backend/test/concrete/TabGenerator.scala
@@ -6,15 +6,15 @@ import org.scalacheck.Gen
 
 trait TabGenerator {
   def antoineDufourTabs: List[Tab] = List(
-    Tab("Antoine Dufour", "Lost in your Eyes", "EACGBD"),
+    antoineDufourTab,
     Tab("Antoine Dufour", "These Moments", "CGEF#BD#")
   )
 
-  def standardTuningTabs: List[Tab] = List(
-    Tab("Kotaro Oshio", "Twilight", "EACGBD"),
-    Tab("Paddy Sun", "Sunflower", "Standard"),
-    Tab("Beethoven", "Fur Elise", "Standard")
-  )
+  def otherTabs: List[Tab] = List(Tab("Kotaro Oshio", "Twilight", "EACGBD")) ::: standardTuningTabs
+
+  val antoineDufourTab = Tab("Antoine Dufour", "Lost in your Eyes", "EACGBD")
+  val standardTuningTabs = List(Tab("Paddy Sun", "Sunflower", "Standard"),
+    Tab("Beethoven", "Fur Elise", "Standard"))
 
   def tabGenerator: Gen[Tab] = ???
 }
--- tabinder-backend/test/services/TabServiceTest.scala
+++ tabinder-backend/test/services/TabServiceTest.scala
@@ -1,10 +1,13 @@
 package services
 
+import cats.Functor
 import concrete.TabRepositoryTest.{HappyPathRepository, MutableRepository}
 import logger.PlayLogger
 import org.scalatest.{FlatSpec, Matchers}
 import cats.instances.all._
 import concrete.TabGenerator
+import eu.timepit.refined.auto._
+import models.Tab
 
 import scala.util.Try
 
@@ -13,10 +16,33 @@ class TabServiceTest extends FlatSpec with Matchers with TabGenerator {
   val allTabs             = standardTuningTabs ::: antoineDufourTabs
   val happyPathRepository = new HappyPathRepository(allTabs)
   val mutableRepository   = new MutableRepository()
+  val service = new TabService[Try](happyPathRepository, logger)
 
   "Tab service" should "return all tabs from repository" in {
-    val service = new TabService[Try](happyPathRepository, logger)
-
     service.getAll().get shouldBe allTabs
+  }
+
+  "Tab service" should "return filtered tabs" in {
+    service.getByArtist("Antoine Dufour").get shouldBe antoineDufourTabs
+  }
+
+  "Tab service" should "return empty list for unknown artists" in {
+    service.getByArtist("unknown artist").get shouldBe List.empty[Tab]
+  }
+
+  "Tab service" should "return the correct song" in {
+    service.getBySong(antoineDufourTab.songName).get shouldBe List(antoineDufourTab)
+  }
+
+  "Tab service" should "return empty list for unknown song" in {
+    service.getBySong("unknown song").get shouldBe List.empty[Tab]
+  }
+
+  "Tab service" should "return correct songs by tuning" in {
+    service.getByTuning("Standard").get shouldBe standardTuningTabs
+  }
+
+  "Tab service" should "return empty list if it doesnt exist" in {
+    service.getByTuning("DADGAD").get shouldBe List.empty[Tab]
   }
 }
