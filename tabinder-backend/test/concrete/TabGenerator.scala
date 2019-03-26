4236aae5bb27b0d2080c18d3bbeac0afdcad6c3c
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-03-26 11:51:27 +0000

    added generator

--- tabinder-backend/test/concrete/TabGenerator.scala
+++ tabinder-backend/test/concrete/TabGenerator.scala
@@ -2,7 +2,11 @@ package concrete
 
 import eu.timepit.refined.auto._
 import models.Tab
+import models.types.Types.Tuning
 import org.scalacheck.Gen
+import eu.timepit.refined.auto._
+
+import scala.util.Random
 
 trait TabGenerator {
   def antoineDufourTabs: List[Tab] = List(
@@ -16,5 +20,29 @@ trait TabGenerator {
   val standardTuningTabs = List(Tab("Paddy Sun", "Sunflower", "Standard"),
     Tab("Beethoven", "Fur Elise", "Standard"))
 
-  def tabGenerator: Gen[Tab] = ???
+  def tabGenerator: Gen[Tab] = {
+    for {
+      artist   <- artistGenerator
+      songname <- songNameGenerator
+      tuning   <- tuningGenerator
+    } yield Tab(artist, songname, tuning)
+  }
+
+  private def tuningGenerator : Gen[Tuning]= {
+    val tuningLetterGen: Gen[String] = Gen.oneOf("A","B","C","D","E","F","G")
+    val inSharpGen: Gen[String]      = Gen.oneOf("", "#")
+
+    def randomTuningGen: Gen[Tuning] = {
+      Gen.const((0 to 5).foldLeft(""){
+        (acc, _) =>
+          val currentString = tuningLetterGen.sample.get + inSharpGen.sample.get
+          acc + currentString
+      })
+    }
+
+    Gen.oneOf(randomTuningGen, Gen.const("Standard": Tuning))
+  }
+
+  private def artistGenerator: Gen[String]   = Gen.alphaStr
+  private def songNameGenerator: Gen[String] = Gen.alphaStr
 }
